5b8e56d435c2e65904df3f1c576e7d6222e14918
ALOK1101 <72982197+ALOK1101@users.noreply.github.com>
2023-01-06 19:55:34 +0100

    Update Źródło.cpp
    Uporządkowany kod, możliwość poruszania się pionkami, na razie bez bicia

--- Źródło.cpp
+++ Źródło.cpp
@@ -13,8 +13,9 @@ using namespace std;
 void intro();
 void boardOut(char tab[8][8]);
 void gameplay(char tab[8][8], int counter);
-bool coordinatesChecker(int y, int x, char tab[8][8], char Player);
-void moves(int y, int x, char tab[8][8], char Player);
+int* coordinatesConverter(string word);
+bool coordinatesChecker(int coordinates[2], char tab[8][8], char Player);
+void moves(int coordinates[2], char tab[8][8], char Player);
 
 bool win();
 int main() {
@@ -27,10 +28,11 @@ int main() {
 						{' ','X',' ','X',' ','X', ' ', 'X'},
 						{'X',' ','X',' ','X',' ', 'X', ' '} };
 	//intro();
-	int RoundCounter=0;
+	int RoundCounter = 0;
 	while (win) {
 		gameplay(board, RoundCounter);
 		RoundCounter++;
+		system("cls");
 	}
 	return 0;
 }
@@ -56,7 +58,7 @@ void boardOut(char tab[8][8]) {
 		cout << y;
 		for (int j = 0; j < 8; j++) {
 			cout << " " << "| " << tab[i][j];
-			sleep(10);
+			sleep(1);
 		}
 		y += 1;
 
@@ -81,58 +83,127 @@ void gameplay(char tab[8][8], int counter) {
 	string coordinates;
 	cout << "Podaj koordynaty pionka, ktorym chcesz sie poruszyc: ";
 	cin >> coordinates;
+	
+	while (!coordinatesChecker(coordinatesConverter(coordinates), tab, Player)) {
+		cout << "Podaj ponownie koordynaty: ";
+		cin >> coordinates;
+	}
+	
+	moves(coordinatesConverter(coordinates), tab, Player);
+	
+	
+}
+int* coordinatesConverter(string coordinates) {
+	int tab[2];
 	int letter, number;
 	if (coordinates[0] >= 65 && coordinates[0] <= 72) letter = coordinates[0] - 65;
 	else if (coordinates[0] >= 97 && coordinates[0] <= 104) letter = coordinates[0] - 97;
-	else cout << "NIeprawidlowe koordynaty\n";
+	else cout << "Nieprawidlowe koordynaty\n";
 	if (coordinates[1] >= 49 && coordinates[1] <= 56) number = coordinates[1] - 49;
-	else cout << "NIeprawidlowe koordynaty\n";
-	cout << endl << number << "\t" << letter;
-	while (!coordinatesChecker(number, letter, tab, Player)) {
-		if (coordinates[0] >= 65 && coordinates[0] <= 72) letter = coordinates[0] - 65;
-		else if (coordinates[0] >= 97 && coordinates[0] <= 104) letter = coordinates[0] - 97;
-		else cout << "NIeprawidlowe koordynaty\n";
-		if (coordinates[1] >= 49 && coordinates[1] <= 56) number = coordinates[1] - 49;
-		else cout << "NIeprawidlowe koordynaty\n";
-		cout << endl << number << "\t" << letter;
-		if (!coordinatesChecker(number, letter, tab, Player) ){
-			cout << "Podaj ponownie koordynaty: ";
-			cin >> coordinates;
+	else cout << "Nieprawidlowe koordynaty\n";
+	//cout << endl << number << "\t" << letter;
+	tab[0] = letter;
+	tab[1] = number;
+	return tab;
+}
+bool coordinatesChecker(int coordinates[2], char tab[8][8], char Player) {
+	int x = coordinates[0], y = coordinates[1];
+	bool flag = false;
+	if (tab[y][x] != Player) return false;
+	if (Player == 'X') {
+		if (y - 1 >= 0 && x + 1 < 8) {
+			if (tab[y - 1][x + 1] == ' ') {
+				flag = true;
+			}
 		}
-	}cout << "Wybierz opcje:";
-	moves(number, letter, tab, Player);
-}
+		if (y - 1 >= 0 && x - 1 >= 0) {
+			if (tab[y - 1][x - 1] == ' ') {
+				flag = true;
+			}
+		}
 
-bool coordinatesChecker(int y, int x, char tab[8][8], char Player) {
+
+	}
+
+	else {
+		if (y + 1 < 8 && x + 1 < 8) {
+			if (tab[y + 1][x + 1] == ' ') {
+				flag = true;
+			}
+		}
+		if (y + 1 < 8 && x - 1 >= 0) {
+			if (tab[y + 1][x - 1] == ' ') {
+				flag = true;
+			}
+		}
+
+	}
+	return flag;
 	
-	if (tab[y][x] == Player) return true;
-	return false;
 }
-void moves(int y, int x, char tab[8][8], char Player) {
+void moves(int coordinates[2], char tab[8][8], char Player) {
+
+	int y = coordinates[1], x = coordinates[0], counter=1;
+	bool flag=false;
+	int moveX, moveY, moveX2, moveY2;
+	cout << "Wybierz opcje ruchu: \n";
 	if (Player == 'X') {
 		if (y - 1 >= 0 && x + 1 < 8) {
 			char x1 = x + 66, y1 = y + 48;
-			if (tab[y - 1][x + 1] == ' ') cout << "mozliwosc ruchu w prawo na pole" <<x1<<y1<<endl;
+			if (tab[y - 1][x + 1] == ' ') {
+				cout << endl << counter << ". mozliwosc ruchu w prawo na pole: " << x1 << y1 << endl;
+				moveX = x + 1;
+				moveY = y - 1;
+				counter++;
+			}
 		}
 		if (y - 1 >= 0 && x - 1 >= 0) {
-			char x1 = x + 64, y1 = y + 48;
-			if (tab[y - 1][x - 1] == ' ') cout << "mozliwosc ruchu w lewo na pole" << x1 << y1 << endl;
+			char x2 = x + 64, y2 = y + 48;
+			if (tab[y - 1][x - 1] == ' '){
+				cout << counter << ". mozliwosc ruchu w lewo na pole: " << x2 << y2 << endl;
+				moveX2 = x - 1;
+				moveY2 = y - 1;
+			}
 		}
-			
+
 
 	}
-	
+
 	else {
 		if (y + 1 < 8 && x + 1 < 8) {
 			char x1 = x + 66, y1 = y + 50;
-			if (tab[y + 1][x + 1] == ' ') cout << "mozliwosc ruchu w prawo na pole" << x1 << y1 << endl;
+			if (tab[y + 1][x + 1] == ' ') {
+				cout <<endl<<counter<< ". mozliwosc ruchu w prawo na pole: " << x1 << y1 << endl;
+				moveX = x + 1;
+				moveY = y + 1;
+				counter++;
+			}
 		}
 		if (y + 1 < 8 && x - 1 >= 0) {
-			char x1 = x + 64, y1 = y + 50;
-			if (tab[y + 1][x - 1] == ' ') cout << "mozliwosc ruchu w lewo na pole" << x1 << y1 << endl;
+			char x2 = x + 64, y2 = y + 50;
+			if (tab[y + 1][x - 1] == ' ') {
+				cout << counter << ". mozliwosc ruchu w lewo na pole: " << x2 << y2 << endl;
+				moveX2 = x - 1;
+				moveY2 = y + 1;
+			}
 		}
 
 	}
+	int move;
+	cin >> move;
+	while (move != 1 && move != 2) {
+		cout << "Nieprawidłowy ruch!\n" << "Podaj ponowanie: ";
+		cin >> move;
+	}
+	if (move == counter-1) {
+		tab[y][x] = ' ';
+		tab[moveY][moveX] = Player;
+	}
+	else {
+		tab[y][x] = ' ';
+		tab[moveY2][moveX2] = Player;
+	}
+	
 }
 bool win() {
 
